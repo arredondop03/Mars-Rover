0e64480ea13567e31841becf1a17118da820ae62
Paola Arredondo <arredondop03@gmail.com>
2019-01-03 09:32:08 -0500

    clean version

--- app.js
+++ app.js
@@ -1,118 +1,100 @@
-// Rover Object Goes Here
-
-var rover = {
-  direction: "N",
+const rover = {
+  direction: 'N',
   x: 0,
-  y:0
-
+  y: 0
 }
-// ==============================================================
-hereIsWhereYouAreAt(rover)
-commands(rover)
-// turnRight(rover)
-// moveForward(rover)
-// moveForward(rover)
-// turnRight(rover)
-// moveForward(rover)
 
 
+let turnLeft = (rover) => {
 
-
-
-
-
-
-// ==============================================================
-function turnLeft(rover) {
-  if (rover.direction === "N") {
-    rover.direction = "W"  
-  } else if(rover.direction === "W") {
-    rover.direction = "S"
-  } else if(rover.direction === "S") {
-    rover.direction = "E"
-  } else if(rover.direction === "E") {
-    rover.direction = "N"
+  switch (rover.direction) {
+    case 'N':
+      rover.direction = 'W'
+      break;
+    case 'W':
+      rover.direction = 'S'
+      break;
+    case 'S':
+      rover.direction = 'W'
+      break;
+    case 'E':
+      rover.direction = 'N'
+      break;
   }
 
   hereIsWhereYouAreAt(rover)
-
-  //console.log("turnLeft was called!");
-  //console.log(rover.direction)
 }
 
-function turnRight(rover){
-  if (rover.direction === "N") {
-    rover.direction = "E"  
-  } else if(rover.direction === "W") {
-    rover.direction = "N"
-  } else if(rover.direction === "S") {
-    rover.direction = "W"
-  } else if(rover.direction === "E") {
-    rover.direction = "S"
+let turnRight = (rover) => {
+
+  switch (rover.direction) {
+    case 'N':
+      rover.direction = 'E'
+      break;
+    case 'E':
+      rover.direction = 'S'
+      break;
+    case 'S':
+      rover.direction = 'W'
+      break;
+    case 'W':
+      rover.direction = 'N'
+      break;
   }
 
   hereIsWhereYouAreAt(rover)
-  //console.log("turnRight was called!");
-  //console.log(rover.direction)
 }
 
-function moveForward(rover){
-  //console.log("x coordinate= " + rover.x)
-  //console.log("y coordinate= " + rover.y)
-  
-  //console.log("moveForward was called")
-  if (rover.direction === "N") {
-    rover.y += 1
-  } else if(rover.direction === "W") {
-    rover.x -= 1  
-  } else if(rover.direction === "S") {
-    rover.y -= 1 
-  } else if(rover.direction === "E") {
-    rover.x += 1 
+let moveForward = (rover) => {
+
+  switch (rover.direction) {
+    case 'N':
+      rover.y += 1
+      break;
+    case 'E':
+      rover.x += 1
+      break;
+    case 'S':
+      rover.y -= 1
+      break;
+    case 'W':
+      rover.x -= 1
+      break;
   }
 
   hereIsWhereYouAreAt(rover)
-
-  //console.log(rover.x + " " + rover.y)
-
 }
 
-function hereIsWhereYouAreAt (rover) {
-  console.log("- ROVER NOW ( " + rover.x + " " + rover.y + " ) , facing " + rover.direction)
-}
 
-function commands(rover) {
- 
+let commands = (rover) =>{
+
   var command = ["r", "m", "m", "r", "m", "l", "m"]
-  
-  var arraySize= command.length
 
-  for(i=0; i<arraySize; i++){
-    if (command[i]==="r"){
-    turnRight(rover)
-    } else if (command[i] === "m"){
-    moveForward(rover)
-    } else if (command[i] === "l") {
-      turnLeft(rover)
+  var arraySize = command.length
+
+  for (i = 0; i < arraySize; i++) {
+
+    switch (command[i]) {
+      case 'r':
+        console.log('turned')
+        turnRight(rover)
+        break;
+      case 'l':
+        console.log('turned')
+        turnLeft(rover)
+        break;
+      case 'm':
+        console.log('moved')      
+        moveForward(rover)
+        break;
     }
   }
-} 
+}
+
+let hereIsWhereYouAreAt = (rover) =>{
+  console.log(`Rover now ${rover.x} ${rover.y}, facing ${rover.direction}`)
+}
 
 
+commands(rover)
 
-// var commandTest = []
-
-
-// var commandTest = [r, m, m, r, m]
-// var arraySize = commandTest.length
-
-// for (i = 0; i < arraySize; i++) {
-//   if (commandTest === commandTest[r]){
-//     turnRight(rover)
-//   } else if (commandTest === l) {
-//     turnLeft(rover)
-//   } else {
-//     moveForward(rover)
-//   } 
-//   hereIsWhereYouAreAt(rover)
-// }
